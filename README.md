13d94abcd10a212cb7de1a1792232ba65b2f1d1e
Kristina Kupscyte <kristina.kupscyte@gmail.com>
2022-02-11 01:18:19 +0000

    Description update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # book-exercise
 Python exercise 
+
+Instructions:
+Create a book class. Initialise each book object with author, title, ISBN, publication year,number of pages. Define str method which displays information about the book. Define search method which searches by book or by author, through a dictionary attribute of the class which tracks the book objects created. Define two subclasses for different genres of books which override the default str method.
--- book_exercise.py
+++ book_exercise.py
@@ -1,11 +1,3 @@
-# Create a book class. Initialise each book object with 
-# author, title, ISBN, publication year, number of pages. 
-# Define str method which displays information about the book.
-# Define search method which searches by book or by author, 
-# through a dictionary attribute of the class which tracks the book objects created. 
-# Define two subclasses for different genres of books which override the default str method. 
-
-
 class Book():
 
     books = {}
